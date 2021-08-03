5b7f7129dd2a9a14be7f0c3c8e538f4ec43c16bd
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-08-03 13:12:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String text = "Hello \nworld";
+        System.out.println(text);
+        String hello = "Hello...";
+        System.out.println(hello);
+        
+    }
+}
